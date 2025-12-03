97b6fa0f053ea31df651a8d5a02da0974693d0c2
mx <sx>
2025-12-03 09:17:40 +0100

    Finalize Lab3 and Lab4 updates

--- Lab2/main2.cpp
+++ Lab2/main2.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "lab2.h"
 
 int main() {
@@ -100,3 +101,48 @@ int main() {
 
     return 0;
 }
+=======
+#ifndef AGRO_H
+#define AGRO_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class AgroCrop {
+private:
+    string Name;
+    string Type;
+    float Area;
+    float Yield;
+
+public:
+    AgroCrop();
+    AgroCrop(string name, string type, float area, float yield);
+    AgroCrop(const AgroCrop& ob);
+
+    void setName(const string& name);
+    void setType(const string& type);
+    void setArea(float area);
+    void setYield(float yield);
+
+    const string& getName() const;
+    string getType() const;
+    float getArea() const;
+    float getYield() const;
+
+    AgroCrop& operator=(const AgroCrop& ob);
+    bool operator==(const AgroCrop& other) const;
+    AgroCrop operator+(const AgroCrop& other) const;
+    int operator[](const char* str);
+    void operator()(string name, string type, float area, float yield);
+
+    void show() const;
+
+    friend bool operator==(const AgroCrop& a, const AgroCrop& b);
+    friend AgroCrop operator+(const AgroCrop& a, const AgroCrop& b);
+    friend ostream& operator<<(ostream& os, const AgroCrop& c);
+    friend istream& operator>>(istream& is, AgroCrop& c);
+};
+
+>>>>>>> b335d96 (Сохраняю текущие изменения перед pull)
